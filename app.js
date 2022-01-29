e435a67d8e427cf2902f1b78d743213ee10ebc2f
pukhraj1999 <pukhrajkhinda@gmail.com>
2022-01-29 17:53:33 +0530

    fixes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(cookieParser());
-app.use("/api/uploads", express.static("uploads"));
+// app.use("/api/uploads", express.static("uploads"));
 //----------------------------------------------------------
 
 //----------Routes-------------------------------------
