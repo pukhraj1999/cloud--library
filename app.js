6d94c945a80091d5275f3bb7cb94b27fc59c2a5a
Pukhraj Singh <54837164+pukhraj1999@users.noreply.github.com>
2022-01-29 23:46:55 +0530

    path fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 // const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
