a4d6447d212e7f515ed6e66393e1fa9b955d362a
pukhraj1999 <pukhrajkhinda@gmail.com>
2022-01-29 23:18:32 +0530

    try again

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("*", (req, res) => {
 //---------------------------------------------------------
 
 //--------Hosting on local server-------------------------------
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
